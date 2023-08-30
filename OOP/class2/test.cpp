fe1b02006fd78bc640f211e4c93c381b9d88376f
jjangdol-DGIST <jjangdol6954@gmail.com>
2023-08-30 04:23:00 +0000

    first commit

---
+++ OOP/class2/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num1, num2;
+    num1 = 1;
+    num2 = 2;
+
+    //print a string
+    cout << "Hello World!" << endl;
+    cout << num1 << "+" << num2 << "=" << num1 + num2 << endl;
+
+    return 0;
+}
