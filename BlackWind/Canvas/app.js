2c2c457f9f8c66d6166650196e896e58290d6a66
jjangdol-DGIST <jjangdol6954@gmail.com>
2023-09-26 11:42:41 +0000

    Canvas study

--- BlackWind/Canvas/app.js
+++ BlackWind/Canvas/app.js
@@ -1,45 +1,51 @@
+//app.js
+console.log("start");
+
 const canvas = document.getElementById("my_canvas");
 const ctx = canvas.getContext("2d");
 
-canvas.width = 1000;
-canvas.height = 1000;
-
-// let x = 10;
-// let y = 10;
-
-// let rectangle_width = 100;
-// let rectangle_height = 100;
-
-// // 문자열을 사용하여서 색 넣기
-// ctx.fillStyle = "rgb(200, 0, 0)";
-// ctx.fillRect(x, y, rectangle_width, rectangle_height);
-
-// x = x + 100;
-// y = y + 100;
-
-
-// ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-// ctx.fillRect(x, y, rectangle_width, rectangle_height);
-
-let my_rectangle = {
-    x: 10,
-    y: 10,
-    width: 100,
-    height: 100,
+let screen = {
+    width: 500,
+    height: 500,
 };
 
-ctx.fillStyle = "rgb(200, 0, 0)";
-ctx.fillRect(my_rectangle.x, my_rectangle.y, my_rectangle.width, my_rectangle.height);
-// top triangle
-ctx.beginPath();
-ctx.moveTo(10, 10);
-ctx.lineTo(50, 10);
-ctx.lineTo(10, 50);
-ctx.fillStyle = "rgb(255, 255, 255)";
-ctx.fill();
-// bottom triangle
-ctx.beginPath();
-ctx.moveTo(110, 110);
-ctx.lineTo(70, 110);
-ctx.lineTo(110, 70);
-ctx.fill();
+canvas.width = screen.width;
+canvas.height = screen.height;
+
+let ball = {
+    x: 100,
+    y: 100,
+    r: 20,
+};
+
+function move_ball(screen, ball, dx, dy) {
+    ball.x += dx;
+    ball.y += dy;
+		
+    if ( ball.x + ball.r > screen.width) {
+        ball.x = screen.width - ball.r;
+    }
+    else if ( ball.x - ball.r < 0) {
+        ball.x = ball.r;
+    }
+    else if (ball.y + ball.r > screen.height) {
+        ball.y = screen.height - ball.r;
+    }
+    else if (ball.y - ball.r < 0) {
+        ball.y = ball.r;
+    }
+
+    return ball;
+}
+
+function draw() {
+    ctx.clearRect(0, 0, screen.width, screen.height);
+    ctx.beginPath();
+    ctx.arc(ball.x, ball.y, ball.r, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+    move_ball(screen, ball, 0, -10)
+}
+
+setInterval(draw, 50);
