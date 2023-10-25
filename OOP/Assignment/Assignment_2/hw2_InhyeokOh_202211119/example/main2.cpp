d1a30926111652f3e0b95d88a8890ba47c5a1adc
jjangdol6954_DGIST <106512138+jjangdol-DGIST@users.noreply.github.com>
2023-10-25 18:03:24 +0900

    hw 2_1 done

--- OOP/Assignment/Assignment_2/hw2_InhyeokOh_202211119/example/main2.cpp
+++ OOP/Assignment/Assignment_2/hw2_InhyeokOh_202211119/example/main2.cpp
@@ -5,9 +5,6 @@ using namespace std;
 class IArticleSub;
 class IArticlePub;
 
-// // Forward Declation
-// void IArticlePub::DetachResponse(IArticleSub* subscriber);
-
 class IArticlePub {
     private:
         string pub_name;
@@ -80,17 +77,14 @@ IArticlePub::IArticlePub(const string name, const string con) : pub_name(name), 
 }
 
 IArticlePub::~IArticlePub() {
-    // 1. 모든 구독자의 publisher 리스트(pub_list)에서 현재 Publisher 제거
+    cout << "IArticlePub Destructor called" << endl;
     for (int i = 0; i < numOfSub; i++) {
         sub_list[i]->DetachResponse(this);
     }
-
-    // 2. 적절한 정보 출력
-    cout << "IArticlePub 소멸자 호출됨" << endl;
 }
 
 void IArticlePub::NotifyAttach(IArticleSub* subscriber) {
-    // 이미 구독자인지 확인
+    // Are you subscriber?
     for (int i = 0; i < numOfSub; i++) {
         if (sub_list[i] == subscriber) {
             cout << "Error: Subscriber " << subscriber->getSubName() << " is already in the publisher's subscriber list." << endl;
@@ -98,42 +92,31 @@ void IArticlePub::NotifyAttach(IArticleSub* subscriber) {
         }
     }
 
-    // 새로운 구독자를 추가
-    if (numOfSub < 5) { // 최대 5명의 구독자까지만 추가 가능
+    // Add neww subscriber
+    if (numOfSub < 5) { // under 5
         sub_list[numOfSub] = subscriber;
         numOfSub++;
 
-        // 대칭적인 연결 (Subscriber의 pub_list에도 Publisher 추가)
-        subscriber->AttachResponse(this);
-
-        // 알림 메시지 출력
         cout << "[Attach] Sub (" << subscriber->getSubName() << "," << subscriber->getSubID() << ") is attached to Pub (" << pub_name << "," << pub_id << ")" << endl;
+        subscriber->AttachResponse(this);
     } else {
         cout << "Error: Maximum number of subscribers reached for the publisher." << endl;
     }
 }
 
 void IArticlePub::NotifyAttachResponse(IArticleSub* subscriber) {
-    cout << "do notify attach response..." << endl;
-
-    // 이미 구독자인지 확인
+    // Are you subscriber?
     for (int i = 0; i < numOfSub; i++) {
-        cout << "aaa" << endl;
         if (sub_list[i] == subscriber) {
             cout << "Error: Subscriber " << subscriber->getSubName() << " is already in the publisher's subscriber list." << endl;
             return;
         }
     }
 
-    // 새로운 구독자를 추가
-    if (numOfSub < 5) { // 최대 5명의 구독자까지만 추가 가능
+    // Add new subscriber
+    if (numOfSub < 5) { // under 5.
         sub_list[numOfSub] = subscriber;
         numOfSub++;
-
-        // 대칭적인 연결 (Subscriber의 pub_list에도 Publisher 추가)
-        subscriber->Attach(this);
-
-        // 알림 메시지 출력
         cout << "[Attach] Sub (" << subscriber->getSubName() << "," << subscriber->getSubID() << ") is attached to Pub (" << pub_name << "," << pub_id << ")" << endl;
     } else {
         cout << "Error: Maximum number of subscribers reached for the publisher." << endl;
@@ -151,13 +134,10 @@ void IArticlePub::NotifyDetach(IArticleSub* subscriber) {
                 sub_list[j] = sub_list[j + 1];
             }
             numOfSub--;
-
             subscriber->DetachResponse(this);
-
             cout << "[Detach] Sub (" << subscriber->getSubName() << "," << subscriber->getSubID() << ") is detached from Pub (" << pub_name << "," << pub_id << ")" << endl;
         }
     }
-
     if (!found) {
         cout << "Error: Subscriber " << subscriber->getSubName() << " is not in the publisher's subscriber list." << endl;
     }
@@ -174,13 +154,9 @@ void IArticlePub::NotifyDetachResponse(IArticleSub* subscriber) {
                 sub_list[j] = sub_list[j + 1];
             }
             numOfSub--;
-
-            subscriber->Detach(this);
-
-            cout << "[Detach] Sub (" << subscriber->getSubName() << "," << subscriber->getSubID() << ") is detached from Pub (" << pub_name << "," << pub_id << ")" << endl;
+            cout << "[Pub] (" << pub_name << "," << pub_id << ") detach [Sub] (" << subscriber->getSubName() << "," << subscriber->getSubID() << ")" << endl;
         }
     }
-
     if (!found) {
         cout << "Error: Subscriber " << subscriber->getSubName() << " is not in the publisher's subscriber list." << endl;
     }
@@ -210,7 +186,7 @@ int IArticlePub::getSubSize() {
 }
 
 void IArticlePub::PrintAllSub() {
-    cout << "All Sub of (" << pub_name << "," << pub_id << "): ";
+    // cout << "All Sub of (" << pub_name << "," << pub_id << "): ";
     for (int i = 0; i < numOfSub; i++) {
         cout << "[" << sub_list[i]->getSubName() << "," << sub_list[i]->getSubID() << "]";
     }
@@ -220,117 +196,92 @@ void IArticlePub::PrintAllSub() {
 //======================================Sub 함수들==========================================
 
 IArticleSub::IArticleSub(const string name) {
-    static int static_sub_counter = 0;
     sub_name = name;
     sub_id = ++static_sub_counter;
 
-    // 정보 출력 (원하는 출력 형식 참조)
     cout << "[Constructor] New Sub Created: (" << sub_name << "," << sub_id << ")" << endl;
 }
 
 IArticleSub::IArticleSub(const string name, IArticlePub* articlePub) {
-    static int static_sub_counter = 0;
     sub_name = name;
     sub_id = ++static_sub_counter;
     cout << "[Constructor] New Sub Created: (" << sub_name << "," << sub_id << ")" << endl;
 
-    // 출판사를 구독자의 목록에 추가
     Attach(articlePub);
-    
-    // 정보 출력 (원하는 출력 형식 참조)
 }
 
 IArticleSub::~IArticleSub() {
-    // 모든 출판사의 구독자 목록에서 구독자 제거
+    cout << "IArticleSub Destructor called" << endl;
+
     for (int i = 0; i < numOfPub; i++) {
         pub_list[i]->NotifyDetachResponse(this);
     }
-
-    // 정보 출력
-    cout << "[Sub] (" << sub_name << "," << sub_id << ") 해지" << endl;
 }
 
 void IArticleSub::Attach(IArticlePub* publisher) {
-    cout << "do attach..." << endl;
-    // 출판사를 구독자의 목록에 추가
     pub_list[numOfPub] = publisher;
     numOfPub++;
-
-    // 대칭적인 관계를 설정하기 위해 출판사에서도 구독자를 추가
+    cout << "[Attach] Pub (" << publisher->getPubName() << "," << publisher->getPubID() << ") is attached to Sub (" << sub_name << "," << sub_id << ")" << endl;
     publisher->NotifyAttachResponse(this);
 }
 
 void IArticleSub::AttachResponse(IArticlePub* publisher) {
-    // 출판사를 구독자의 목록에 추가
     pub_list[numOfPub] = publisher;
     numOfPub++;
-}
+cout << "[Attach] Pub (" << publisher->getPubName() << "," << publisher->getPubID() << ") is attached to Sub (" << sub_name << "," << sub_id << ")" << endl;
+}   
 
 void IArticleSub::Detach(IArticlePub* p_pub) {
     int index = -1;
-    
-    // 출판사를 구독자의 목록에서 찾아 제거
+    // remove
     for (int i = 0; i < numOfPub; i++) {
         if (pub_list[i] == p_pub) {
             index = i;
             break;
         }
     }
-    
     if (index != -1) {
-        // 출판사를 제거하고 목록을 재조정
         for (int i = index; i < numOfPub - 1; i++) {
             pub_list[i] = pub_list[i + 1];
         }
         numOfPub--;
-        
-        // 대칭적인 관계를 설정하기 위해 출판사의 NotifyDetachResponse 함수 호출
+        cout << "[Sub] (" << sub_name << "," << sub_id << ") unsubscribes [Pub] (" << p_pub->getPubName() << "," << p_pub->getPubID() << ")" << endl;
+
         p_pub->NotifyDetachResponse(this);
     } else {
-        // 출판사가 목록에 존재하지 않는 경우 오류 메시지 출력
-        cout << "Error: 출판사가 구독자의 목록에 존재하지 않습니다." << endl;
+        cout << "Error: There is no publisher in subscribed list." << endl;
     }
 }
 
 void IArticleSub::DetachResponse(IArticlePub* p_pub) {
-    // int index = -1;
-    // int numOfSub = p_pub->getSubSize();
-    // int sub_list = p_pub.sub;
-    // // 구독자를 출판사의 목록에서 찾아 제거
-    // for (int i = 0; i < numOfSub; i++) {
-    //     if (sub_list[i] == p_pub) {
-    //         index = i;
-    //         break;
-    //     }
-    // }
-
-    // if (index != -1) {
-    //     // 구독자를 제거하고 목록을 재조정
-    //     for (int i = index; i < numOfSub - 1; i++) {
-    //         sub_list[i] = sub_list[i + 1];
-    //     }
-    //     numOfSub--;
-    // }
-
-    this->Detach(p_pub);
+        int index = -1;
+    
+    for (int i = 0; i < numOfPub; i++) {
+        if (pub_list[i] == p_pub) {
+            index = i;
+            break;
+        }
+    }
+    if (index != -1) {
+        for (int i = index; i < numOfPub - 1; i++) {
+            pub_list[i] = pub_list[i + 1];
+        }
+        numOfPub--;
+        cout << "[Sub] (" << sub_name << "," << sub_id << ") unsubscribes [Pub] (" << p_pub->getPubName() << "," << p_pub->getPubID() << ")" << endl;
+    } else {
+        cout << "Error: There is no publisher in subscribed list." << endl;
+    }
 }
 
 void IArticleSub::Update(IArticlePub* publisher, const string contents) {
     recent_article_pub = publisher;
     recent_article_contents = contents;
     
-    // 최신 정보를 출력하는 PrintContents() 함수 호출
     PrintContents();
 }
 
 void IArticleSub::PrintContents() {
-    if (recent_article_pub != nullptr) {
-        cout << "최신 기사 정보:" << endl;
-        cout << "출판사: " << recent_article_pub->getPubName() << endl;
-        cout << "내용: " << recent_article_contents << endl;
-    } else {
-        cout << "아직 최신 기사가 없습니다." << endl;
-    }
+    cout << "Sub (" << sub_name << "," << sub_id << ")'s latest subscribed news is \"" << recent_article_contents << "\" by " << recent_article_pub->getPubName() << endl;
 }
 
 string IArticleSub::getSubName() {
@@ -346,7 +297,6 @@ int IArticleSub::getPubSize() {
 }
 
 void IArticleSub::PrintAllPub() {
-    cout << "구독 중인 모든 출판사 목록: ";
     for (int i = 0; i < numOfPub; i++) {
         cout << "[" << pub_list[i]->getPubName() << "," << pub_list[i]->getPubID() << "]";
     }
@@ -363,81 +313,77 @@ int main() {
 
     //Jenny subscribe DGIST, BBC
     IArticleSub* jennySub = new IArticleSub("Jenny", dgistPub); 
-    cout << 'a' << endl;
     bbcPub->NotifyAttach(jennySub);
-    cout << 'a' << endl;
     cout << endl;
 
-    // //Tom subscribe BBC, CNN
-    // IArticleSub* tomSub = new IArticleSub("Tom");
-    // bbcPub->NotifyAttach(tomSub); 
-    // cnnPub->NotifyAttach(tomSub);
-    // cout << endl;
+    //Tom subscribe BBC, CNN
+    IArticleSub* tomSub = new IArticleSub("Tom");
+    bbcPub->NotifyAttach(tomSub); 
+    cnnPub->NotifyAttach(tomSub);
+    cout << endl;
 
-    // //Kate subscribe DGIST, BBC, CNN
-    // IArticleSub* kateSub = new IArticleSub("Kate", dgistPub);
-    // bbcPub->NotifyAttach(kateSub);
-    // kateSub->Attach(cnnPub);
-    // cout << endl;
+    //Kate subscribe DGIST, BBC, CNN
+    IArticleSub* kateSub = new IArticleSub("Kate", dgistPub);
+    bbcPub->NotifyAttach(kateSub);
+    kateSub->Attach(cnnPub);
+    cout << endl;
 
-    // cout << "All Sub of (" << dgistPub->getPubName() << "," << dgistPub->getPubID() << "): ";
-    // dgistPub->PrintAllSub();
+    cout << "All Sub of (" << dgistPub->getPubName() << "," << dgistPub->getPubID() << "): ";
+    dgistPub->PrintAllSub();
 
-    // cout << "All Sub of (" << bbcPub->getPubName() << "," << bbcPub->getPubID() << "): ";
-    // bbcPub->PrintAllSub();
+    cout << "All Sub of (" << bbcPub->getPubName() << "," << bbcPub->getPubID() << "): ";
+    bbcPub->PrintAllSub();
 
-    // cout << "All Sub of (" << cnnPub->getPubName() << "," << cnnPub->getPubID() <<
-    // "): "; 
+    cout << "All Sub of (" << cnnPub->getPubName() << "," << cnnPub->getPubID() << "): "; 
+    cnnPub->PrintAllSub(); 
+    cout << endl;
 
-    // cnnPub->PrintAllSub(); 
-    // cout << endl;
+    kateSub->Detach(bbcPub); 
+    cout << endl;
 
-    // kateSub->Detach(bbcPub); 
-    // cout << endl;
+    cout << "All Pub of (" << jennySub->getSubName() << "," << jennySub->getSubID() << "): ";
+    jennySub->PrintAllPub();
 
-    // cout << "All Pub of (" << jennySub->getSubName() << "," << jennySub->getSubID() << "): ";
-    // jennySub->PrintAllPub();
+    cout << "All Pub of (" << tomSub->getSubName() << "," << tomSub->getSubID() << "): ";
+    tomSub->PrintAllPub();
 
-    // cout << "All Pub of (" << tomSub->getSubName() << "," << tomSub->getSubID() << "): ";
-    // tomSub->PrintAllPub();
+    cout << "All Pub of (" << kateSub->getSubName() << "," << kateSub->getSubID() << "): ";
+    kateSub->PrintAllPub(); cout << endl;
 
-    // cout << "All Pub of (" << kateSub->getSubName() << "," << kateSub->getSubID() << "): ";
-    // kateSub->PrintAllPub(); cout << endl;
+    cout << "=========DGIST Notify ===========" << endl; 
+    dgistPub->updatePubContents("Welcome New DGIST students"); 
+    cout << endl;
 
-    // cout << "=========DGIST Notify ===========" << endl; 
-    // dgistPub->updatePubContents("Welcome New DGIST students"); 
-    // cout << endl;
+    cout << "=========BBC Notify ===========" << endl; 
+    bbcPub->updatePubContents("Mr. Son scored at Tottenham"); 
+    cout << endl;
 
-    // cout << "=========BBC Notify ===========" << endl; 
-    // bbcPub->updatePubContents("Mr. Son scored at Tottenham"); 
-    // cout << endl;
+    cout << "=========CNN Notify ===========" << endl; 
+    cnnPub->updatePubContents("New York city celebrates Christmas"); 
+    cout << endl;
 
-    // cout << "=========CNN Notify ===========" << endl; 
-    // cnnPub->updatePubContents("New York city celebrates Christmas"); 
-    // cout << endl;
+    cout << "=========DELETING [tomSub]===========" << endl; 
+    delete tomSub;
+    cout << endl;
 
-    // cout << "=========DELETING [tomSub]===========" << endl; 
-    // delete tomSub;
-    // cout << endl;
+    cout << "=========DGIST Notify ===========" << endl; 
+    dgistPub->updatePubContents("Welcome New DGIST students"); 
+    cout << endl;
 
-    // cout << "=========DGIST Notify ===========" << endl; 
-    // dgistPub->updatePubContents("Welcome New DGIST students"); 
-    // cout << endl;
+    cout << "=========BBC Notify ===========" << endl; 
+    bbcPub->updatePubContents("Mr. Son scored at Tottenham"); 
+    cout << endl;
 
-    // cout << "=========BBC Notify ===========" << endl; 
-    // bbcPub->updatePubContents("Mr. Son scored at Tottenham"); 
-    // cout << endl;
+    cout << "=========CNN Notify ===========" << endl; 
+    cnnPub->updatePubContents("New York city celebrates Christmas"); 
+    cout << endl;
 
-    // cout << "=========CNN Notify ===========" << endl; 
-    // cnnPub->updatePubContents("New York city celebrates Christmas"); 
-    // cout << endl;
-
-    // cout << "=========Delete all others ===========" << endl; 
-    // delete dgistPub;
-    // delete bbcPub;
-    // delete cnnPub;
-    // delete jennySub;
-    // delete kateSub;
+    cout << "=========Delete all others ===========" << endl; 
+    delete dgistPub;
+    delete bbcPub;
+    delete cnnPub;
+    delete jennySub;
+    delete kateSub;
     
     return 0; 
 }
---
+++ OOP/Assignment/Assignment_2/hw2_InhyeokOh_202211119/example/tempCodeRunnerFile.cpp
@@ -0,0 +1,2 @@
+iber의 pub_list에도 Publisher 추가)
+        // subscriber->Attach(this);
